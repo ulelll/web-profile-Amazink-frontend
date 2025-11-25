d76b0b0e64f30e926e136681bea1d465d2835f27
rerruu <darell.liebert@gmail.com>
2025-11-25 16:01:54 +0700

    feat: update React version and add news upload and user management pages
    - Downgraded React and ReactDOM to version 18.3.1 in package.json
    - Added react-quill for rich text editing in news upload
    - Implemented NewsUploadPage component for uploading news articles
    - Created UserManagementPage component for managing users with search and filter functionality
    - Updated HeaderLandingPage with additional entries

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import PublicLayout from './layouts/public_layout.jsx'
 import Dashboard from './features/admin/pages/dashboard.jsx'
 import HeaderLandingPage from './features/admin/admin-layouts/cms/header_landing_page.jsx'
+import NewsUploadPage from './features/admin/admin-layouts/cms/custom_news.jsx'
+import UserManagementPage from './features/admin/admin-layouts/cms/user_management.jsx'
 
 function App() {
   return (
        // <PublicLayout />
        //<Dashboard />
-        <HeaderLandingPage />
-  )
-}
-export default App
+      // <HeaderLandingPage />
+       // <NewsUploadPage />
+      <UserManagementPage/>
+    )
+  }
+  export default App
