b662821178eaf21859c46e16430464ad94ef307f
KasiarAffan <kaisaraffan1@gmail.com>
2025-12-15 09:35:08 +0700

    create detail page for news

--- src/app/routes/PublicRouters.jsx
+++ src/app/routes/PublicRouters.jsx
@@ -1,3 +1,4 @@
+import NewsDetailPage from "@/features/public/section/news/Detail-page";
 import PublicLayout from "@/layouts/public_layout.jsx";
 import { Route } from "react-router-dom";
 
@@ -5,7 +6,7 @@ export default function PublicRoutes() {
     return (
         <>
             <Route path="/" element={<PublicLayout />} />
-            <Route path="/news/:id" element={<PublicLayout />} />
+            <Route path="/news/:id" element={<NewsDetailPage />} />
         </>
     );
 }
